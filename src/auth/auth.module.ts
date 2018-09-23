3634417006a60a5ee0fd816627ef859555bfd825
Johannes Voigt <jvoigtmail@gmail.com>
2018-09-23 15:24:02 +0200

    Listing 3 auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { HttpStrategy } from './http.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule],
+  providers: [AuthService, HttpStrategy],
 })
 export class AuthModule {}
