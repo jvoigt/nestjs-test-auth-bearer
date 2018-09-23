c066858cd9875c11f9203ceeddb1067a6fda6726
Johannes Voigt <jvoigtmail@gmail.com>
2018-09-23 15:37:50 +0200

    Listing 4 add AuthGuard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseGuards(AuthGuard('bearer'))
   root(): string {
     return this.appService.root();
   }
